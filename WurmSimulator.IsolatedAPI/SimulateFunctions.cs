e70c8e4a53d5c16ec57820ea390cfc615cd3af80
Andrew Webb <1918425+webba@users.noreply.github.com>
2023-11-03 19:49:13 +0000

    Switched to Isolated Api

---
+++ WurmSimulator.IsolatedAPI/SimulateFunctions.cs
@@ -0,0 +1,61 @@
+using System.Net;
+using System.Text.Json;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using WurmSimulator.Lib;
+
+namespace WurmSimulator.IsolatedAPI
+{
+    public class SimulateFunctions
+    {
+        private readonly ILogger _logger;
+
+        public SimulateFunctions(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<SimulateFunctions>();
+        }
+
+        [Function("Example")]
+        public HttpResponseData Example([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# Running Example.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "application/json");
+
+            response.WriteString(JsonSerializer.Serialize(new Scenario()));
+            return response;
+        }
+
+        [Function("Simulate")]
+        public async Task<HttpResponseData> Simulate([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# Running Simulate.");
+
+            var scenario = await JsonSerializer.DeserializeAsync<Scenario>(req.Body);
+
+
+            if (scenario == null)
+            {
+                var bresponse = req.CreateResponse(HttpStatusCode.BadRequest);
+                return bresponse;
+            }
+
+            int maxSims = 100_000;
+            if (scenario.Simulations > maxSims)
+            {
+                scenario.Simulations = maxSims;
+            }
+
+            var results = WurmSkillUtil.RunScenario(scenario);
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "application/json");
+
+
+            response.WriteString(JsonSerializer.Serialize(results));
+            return response;
+        }
+    }
+}
